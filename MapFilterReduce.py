9e78df578ca6d2ec18ed9d785865ee2702346f7f
Ciprian Nica <ciprian@MacBook-Pro-de-Ciprian.local>
2020-11-01 11:00:48 +0100

    map. filter, reduce

--- MapFilterReduce.py
+++ MapFilterReduce.py
@@ -1,6 +1,4 @@
-
 from functools import reduce
-
 
 lista = [1, 3, -1, 15, 9, 10]
 
@@ -10,3 +8,10 @@ listaPares = filter(lambda x: x%2 == 0, lista)
 sumaTodos = reduce(lambda x, y: x+y, lista)
 
 suma100 = reduce(lambda x, y: x+y, range(101))
+
+print(lista)
+print("lista de dobles: ", list(listaDoble))
+print("lista pares: ", list(listaPares))
+
+print("suma todos: ", sumaTodos)
+print("suma100: ", suma100)
